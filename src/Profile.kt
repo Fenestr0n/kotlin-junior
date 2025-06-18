5d9d08fd06d4e4c1911c3f4337c9ca0e78db13c1
Roman Kozhin <kozhin.rm@mail.ru>
2025-06-18 16:35:27 +0300

    Чтение из консоли. Переменные

---
+++ src/Profile.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Input your name: ")
+    val name = readln()
+
+    println("Input your age: ")
+    val age = readln()
+
+    println("Input your gender: ")
+    val gender = readln()
+
+    println("Input your weight")
+    val weight = readln()
+
+    println("Name: " + name + " Age: " + age + " Gender: " + gender + " Weight: " + weight)
+
+}
