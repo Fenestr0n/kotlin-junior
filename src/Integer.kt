93d68f9b63bc9f44a75c7e0e45a90f6706888c0e
Roman Kozhin <kozhin.rm@mail.ru>
2025-06-24 12:30:15 +0300

    Целочисленный тип Int

---
+++ src/Integer.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    print("How many apples does John have? ")
+    val john = readln().toInt()
+    val nick = john + 7
+    val sum = john + nick
+    println("John and Nick have $sum apples")
+
+    val first = readln().toInt()
+    val second= readln().toInt()
+    val result = first * second
+    println(result)
+}
